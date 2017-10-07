b830331ee726f1936163a5bb9ef725c325ac31a9
Tommi Mantila <tommi.mantila@spinfire.fi>
2017-10-07 19:19:16 +0300

    Frontend changes

--- frontend/js/poll.js
+++ frontend/js/poll.js
@@ -2,11 +2,12 @@
 				$.get('/vessatutka/api/motion/')
 				.done(function(data) {
 					if(data.length && moment(data[0].timestamp).diff(moment()) < 30000) {
-						$('.header').html('Vessa on varattu');
+						$('.header').removeClass('free').addClass('reserved').html('Vessa on varattu');
 					}
 					else {
-						$('.header').html('Vessa on vapaa');
+						$('.header').removeClass('reserved').addClass('free').html('Vessa on vapaa');
 					}
+					setTimeout(checkBathroom, 10000);
 				})
 				.fail(function() {
 					setTimeout(checkBathroom, 10000);
